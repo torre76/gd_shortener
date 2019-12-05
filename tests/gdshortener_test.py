2d504e13cf875863a578fa17f96601baaf0f215a
Gian Luca Dalla Torre <gianluca.dallatorre@me.com>
2019-12-06 00:10:09 +0100

    Fix tests on Python 3
    Also some PEP-8 applied.

--- tests/gdshortener_test.py
+++ tests/gdshortener_test.py
@@ -1,76 +1,122 @@
-import gdshortener
 import unittest
+import logging
+from logging.config import dictConfig
+
+import gdshortener
 
 
 class GDShortenerTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        dictConfig({
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "logformatter": {
+                    "format": "%(asctime)s - [%(levelname)s] - %(message)s",
+                    "datefmt": "%Y-%m-%d %H:%M:%S"
+                },
+            },
+            "handlers": {
+                "loghandler": {
+                    "class": "logging.StreamHandler",
+                    "level": logging.DEBUG,
+                    "formatter": "logformatter"
+                }
+            },
+            "loggers": {
+                # Default logs (info in production or debug in development)
+                "": {
+                    "level": logging.DEBUG,
+                    "handlers": ("loghandler",),
+                    "propagate": False
+                },
+                "nose": {
+                    "level": logging.WARNING,
+                    "handlers": ("loghandler",),
+                    "propagate": False
+                },
+                "urllib3.connectionpool": {
+                    "level": logging.WARNING,
+                    "handlers": ("loghandler",),
+                    "propagate": False
+                },
+            }
+        })
 
     def setUp(self):
         self._tested = gdshortener.GDBaseShortener(timeout=30)
         self._tested_is = gdshortener.ISGDShortener(timeout=30)
         self._tested_v = gdshortener.VGDShortener()
-
-    def tearDown(self):
-        pass
+        self._logger = logging.getLogger()
 
     def testShorten(self):
         shortened_url, stat_url = self._tested.shorten(
-                                             url = "http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14")
+            url="http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14")
         self.assertIsNotNone(shortened_url)
         self.assertIsNone(stat_url)
-        print "Url obtained: [{0}]".format(shortened_url)
+        self._logger.info("Url obtained: [{0}]".format(shortened_url))
 
     def testShortenWithCustomUrl(self):
-        self.assertRaises(gdshortener.GDShortURLError, self._tested.shorten, "http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14", 'GooglE')
+        self.assertRaises(gdshortener.GDGenericError, self._tested.shorten,
+                          "http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14", 'GooglE')
 
     def testShortenWithLog(self):
         shortened_url, stat_url = self._tested.shorten(
-                                             url = "http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14",
-                                             log_stat= True)
+            url="http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14",
+            log_stat=True)
         self.assertIsNotNone(shortened_url)
         self.assertIsNotNone(stat_url)
-        print "Url obtained: [{0}] - Stat url obtained: [{1}]".format(shortened_url, stat_url)
-        
+        self._logger.info(
+            "Url obtained: [{0}] - Stat url obtained: [{1}]".format(shortened_url, stat_url))
+
     def testLookupShortenUrl(self):
         shortened_url, stat_url = self._tested.shorten(
-                                             url = "http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14")
+            url="http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14")
         self.assertIsNotNone(shortened_url)
         self.assertIsNone(stat_url)
         original_url = self._tested.lookup(shortened_url)
         self.assertIsNotNone(original_url)
-        print "Url obtained: [{0}] - Lookup url: [{1}]".format(shortened_url, original_url)
-        
+        self._logger.info(
+            "Url obtained: [{0}] - Lookup url: [{1}]".format(shortened_url, original_url))
+
     def testVShorten(self):
         shortened_url, stat_url = self._tested_v.shorten(
-                                             url = "http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14")
+            url="http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14")
         self.assertIsNotNone(shortened_url)
         self.assertIsNone(stat_url)
-        print "Url obtained: [{0}]".format(shortened_url)
+        self._logger.info("Url obtained: [{0}]".format(shortened_url))
 
     def testVShortenWithCustomUrl(self):
-        self.assertRaises(gdshortener.GDShortURLError, self._tested.shorten, "http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14", 'GooglE')
+        self.assertRaises(gdshortener.GDGenericError, self._tested.shorten,
+                          "http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14", 'GooglE')
 
     def testVShortenWithLog(self):
         shortened_url, stat_url = self._tested_v.shorten(
-                                             url = "http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14",
-                                             log_stat= True)
+            url="http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14",
+            log_stat=True)
         self.assertIsNotNone(shortened_url)
         self.assertIsNotNone(stat_url)
-        print "Url obtained: [{0}] - Stat url obtained: [{1}]".format(shortened_url, stat_url)
-        
+        self._logger.info(
+            "Url obtained: [{0}] - Stat url obtained: [{1}]".format(shortened_url, stat_url))
+
     def testVLookupShortenUrl(self):
         shortened_url, stat_url = self._tested_v.shorten(
-                                             url = "http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14")
+            url="http://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=louth&sll=53.800651,-4.064941&sspn=33.219383,38.803711&ie=UTF8&hq=&hnear=Louth,+United+Kingdom&ll=53.370272,-0.004034&spn=0.064883,0.075788&z=14")
         self.assertIsNotNone(shortened_url)
         self.assertIsNone(stat_url)
         original_url = self._tested_v.lookup(shortened_url)
         self.assertIsNotNone(original_url)
-        print "Url obtained: [{0}] - Lookup url: [{1}]".format(shortened_url, original_url)
+        self._logger.info(
+            "Url obtained: [{0}] - Lookup url: [{1}]".format(shortened_url, original_url))
 
     def testBrokenSSLCertificate(self):
-        shortened_url, stat_url = self._tested.shorten(url="https://expired.badssl.com/", verify_ssl=False)
+        shortened_url, stat_url = self._tested.shorten(
+            url="https://expired.badssl.com/", verify_ssl=False)
         self.assertIsNotNone(shortened_url)
         self.assertIsNone(stat_url)
-        print "Url obtained: [{0}]".format(shortened_url)
+        self._logger.info("Url obtained: [{0}]".format(shortened_url))
 
 
 if __name__ == "__main__":
