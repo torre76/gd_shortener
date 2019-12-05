870300c9e345c6f4f61dd11b0b394b2f4da376b0
Gian Luca Dalla Torre <gianluca.dallatorre@me.com>
2019-12-05 21:09:01 +0100

    Prepare for release 1.0.0 with Python 3 support

--- setup.py
+++ setup.py
@@ -5,26 +5,26 @@ long_description = codecs.open('README.rst', "r").read()
 
 setup(
     name="gdshortener",
-    version="0.0.4",
+    version="1.0.0",
     author="Gian Luca Dalla Torre",
     author_email="gianluca@gestionaleauto.com",
     description=("A module that provides access to .gd URL Shortener"),
     license="LGPL",
     keywords="url shortener gd",
     url="https://github.com/torre76/gd_shortener",
-    download_url="https://github.com/torre76/gd_shortener/tarball/0.0.4",
+    download_url="https://github.com/torre76/gd_shortener/tarball/1.0.0",
     packages=['gdshortener'],
     long_description=long_description,
     package_data={
         '': ['README.rst'],
     },
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 2 :: Only"
+        "Programming Language :: Python :: 3"
     ],
     install_requires=[
         "requests >= 2.21.0"
